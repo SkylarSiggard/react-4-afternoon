7d55bad7115a2e707786ff178239b390ec9f578f
skylar siggard <siggardskylar@gmail.com>
2019-09-11 09:02:29 -0600

    on step 7

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 export default class Student extends Component {
   constructor() {
     super()
-
+    this.state = { 
+    studentInfo: {} 
+    }
   }
-
   render() {
     return (
       <div className="box">
